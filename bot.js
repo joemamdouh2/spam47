827e9aa652b98f6ba11bfb551801c44807d57978
joemamdouh2 <39736521+joemamdouh2@users.noreply.github.com>
2018-12-01 19:00:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,14 +57,14 @@ client.on('ready', async() => {
 var server = "512693975363223573"; 
 var channel = "513991573772697602";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('sdsdoasmdoamsdoamsdom')
+    client.guilds.get(server).channels.get(channel).send('eam , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dreamdoamsdom')
     },305);
 })
 client.on('ready', async() => {
 var server = "512693975363223573"; 
 var channel = "513991573772697602";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('sdsdoasmdoamsdoamsdom')
+    client.guilds.get(server).channels.get(channel).send('sdsdoasm Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Drdoamsdoamsdom')
     },305);
 })
 
